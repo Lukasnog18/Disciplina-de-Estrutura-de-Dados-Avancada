c248bde0ba912e0d116507801c5fa64b17a17440
Lukasnog18 <luksnogueira18@gmail.com>
2017-03-27 12:58:45 -0300

    Update HeapMaximo.java

--- src/br/ufc/quixada/eda/listaprioridades/HeapMaximo.java
+++ src/br/ufc/quixada/eda/listaprioridades/HeapMaximo.java
@@ -7,6 +7,7 @@ import java.util.List;
  * @author fabio
  *
  */
+
 public class HeapMaximo {
 	private int nMaximo = 0;
 	private int vetor[] = null;
@@ -19,11 +20,11 @@ public class HeapMaximo {
 	
 	public void contruir(List<Integer> entrada){
         for(int i = 0; i < entrada.size(); i++){
-            this.vetor[i] = entrada.get(i);
+            vetor[i] = entrada.get(i);
         }
-        this.n = entrada.size();
+        n = entrada.size();
         
-        for(int i = (this.n / 2); i >= 0; i--){
+        for(int i = (n / 2); i >= 0; i--){
             descer(i);
         }
 	}
@@ -31,7 +32,7 @@ public class HeapMaximo {
 	private void subir(int i){
             int p = (i / 2);
             if(p >= 1){
-                if(this.vetor[p] < vetor[i]){
+                if(vetor[p] < vetor[i]){
                     int aux = vetor[i];
                     vetor[i] = vetor[p];
                     vetor[p] = aux;
@@ -42,55 +43,45 @@ public class HeapMaximo {
 	
 	private void descer(int i){
 		int j = 2 * (i + 1);
-		if(j <= this.n){
+		if(j <= n){
 			j--;
-			if(j + 1 < this.n && this.vetor[j] < this.vetor[j + 1]) j++;
-			if(this.vetor[i] < this.vetor[j]){
-				int aux = this.vetor[i];
-				this.vetor[i] = this.vetor[j];
-				this.vetor[j] = aux;
+			if(j + 1 < n && vetor[j] < vetor[j + 1]) j++;
+			if(vetor[i] < vetor[j]){
+				int aux = vetor[i];
+				vetor[i] = vetor[j];
+				vetor[j] = aux;
 				descer(j);
 			}
 		}
-//            int f = (i * 2);
-//            if(f <= n && f + 1 <= n){
-//                    if(this.vetor[f + 1]  > this.vetor[f]) f++;
-//            }
-//            if(this.vetor[f] > this.vetor[i]){
-//                int aux = this.vetor[f];
-//                this.vetor[f] = this.vetor[i];
-//                this.vetor[i] = aux;
-//                descer(f);
-//            }
 	}
 	
 	public int getMaximaPrioridade(){
-		return this.vetor[0];
+		return vetor[0];
 	}
 	
 	public int remove(){
-            if(this.n > 0){
-                int aux = this.vetor[1];
-                this.vetor[1] = this.vetor[this.n];
-                this.n--;
+            if(n > 0){
+                int aux = vetor[1];
+                vetor[1] = vetor[n];
+                n--;
                 descer(1);
                 return aux;
             }
-		return -1;
+		return 0;
 	}	
 	
 	public void inserir(int prioridade){
-            if(this.n < nMaximo){
-                this.vetor[this.n + 1] = prioridade;
-                this.n++;
-                subir(this.n);
+            if(n < nMaximo){
+                vetor[n + 1] = prioridade;
+                n++;
+                subir(n);
             }
 	}
 	
 	public void alterarPrioridade(int prioridade, int novaPrioridade){
-            for(int i = 1; i <= this.n; i++){
-                if(this.vetor[i] == prioridade){
-                    this.vetor[i] = novaPrioridade;
+            for(int i = 1; i <= n; i++){
+                if(vetor[i] == prioridade){
+                    vetor[i] = novaPrioridade;
                     if(novaPrioridade > prioridade){
                         subir(i);
                     } else{
